68170965adc1cbd55fa930b99e846880bccd66e2
Vaishnav-NT <vaishnav.nt.10@gmail.com>
2023-10-03 17:28:02 +0530

    Initial setup completed

---
+++ src/test/java/com/kv/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kv.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
