1f328178769984d26d81f036d5b89ae94430ab04
Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
2023-10-04 09:37:24 +0530

    added javadoc to authservice app

--- src/main/java/com/kv/authservice/AuthServiceApplication.java
+++ src/main/java/com/kv/authservice/AuthServiceApplication.java
@@ -3,9 +3,19 @@ package com.kv.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the AuthService application.
+ * This class serves as the entry point for starting the application.
+ */
 @SpringBootApplication()
 public class AuthServiceApplication {
 
+     /**
+     * The main method of the AuthService application.
+     * When executed, it initializes and runs the Spring Boot application.
+     *
+     * @param args An array of command-line arguments passed to the application (if any).
+     */
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
